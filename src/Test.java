4bdbddeac0dad6264f4dcca79834388a59765ad8
MansiMoliya <mansibhalala7@gmail.com>
2024-01-27 15:14:27 +0000

    commit5

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main(String[] args) {
+        System.out.println("My name is mansi,hello");
         System.out.println("My name is mansi,How r u?");
     }
 }
